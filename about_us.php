0cc1be441ed186ad695602b76404b99cf7224a8f
Prathmesh Sawant <168191051+Prathmesh1545@users.noreply.github.com>
2024-12-02 08:36:03 +0530

    Add files via upload

---
+++ about_us.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>About Us</title>
+<link rel="stylesheet" href="css/about_us.css" />
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>About Us Navigation Bar</title>
+</head>
+
+<link rel="stylesheet" href="css/header_navigationbar.css" />
+
+<body>
+<div>
+<img src="image/home header.jpg" width="100%" height="250px" />
+</div>
+<table class="table">
+<tr>
+<td>
+<ul class="ul1">
+	<div class="dropdown">
+	<li class="li_image"><img src="image/WE23.png" width="55px" height="55px" style="padding-left:15px;padding-right:15px" /></li>
+    </div>
+    <div class="dropdown">
+    <li class="li1"><a href="index.php"><span class="span1">Home</span></a></li>
+		<div class="dropdown-content">
+    		<a href="index.php#welcome">Main</a>
+    		<a href="index.php#howto">Becoming member</a>
+        </div>
+    </div>
+    <div class="dropdown">
+    <li class="li1 active"><a href="about_us.php" name="about" onclick="change()"><span class="span1">About Us</span></a></li>
+    </div>
+    <div class="dropdown">
+  	<li class="li1"><a href="news.php"><span class="span1">News</span></a></li>
+    	<div class="dropdown-content">
+    		<a href="news.php#announcements">Announcement Board</a>
+    		<a href="news.php#events">Events</a>
+   		</div>
+    </div>
+    <div class="dropdown">
+  	<li class="li1"><a href="eforum.php"><span class="span1">E-Forum</span></a></li>
+    </div>
+    <div class="dropdown">
+    <div class="dropdown">
+  	<li class="li1"><a href="search_alumni.php"><span class="span1">Search Alumni</span></a></li>
+    </div>
+    <li class="li1"><a href="contact_us.php"><span class="span1">Contact Us</span></a></li>
+    	<div class="dropdown-content">
+    		<a href="contact_us.php#contact">Contact</a>
+    		<a href="contact_us.php#faq">FAQ</a>
+        </div>
+    </div>
+    <li class="li2"><a href="register.html"><span class="span1">Register</span></a></li>
+    <li class="li2"><a href="login.html"><span class="span1">Login</span></a></li>
+</ul>
+</td>
+</tr>
+</table>
+
+<button id="about_totop" onclick="topFunction()"><img src="image/top.jpg" width="50px" height="50px"/></button>
+<script src= src="javascript/about_totop.js"></script>
+
+</body>
+</html></head>
+
+<body>
+<div id="history">
+<p class="p1">Goverment Polytechnic Mumbai Alumni Management</p>
+<p class="p3">The GPM Alumni Association supervises the graduate programs in GPM. 
+	This includes administering the admission, appointment of Alumnis, Members reviews, 
+	provision of financial assistance and assessment. </p><br />
+<p class="p2">FUNCTION OF  ALUMNI  MANAGEMENT  SYSTEM (AMS)</p>
+<p class="p3">The function of AMS includes the following:
+<ul class="ul2">
+	<li>Promoting GPM’s graduate programs</li>
+	<li>Facilitate the appointment of Alumni's after graduation</li>
+	<li>Oversees the provision of financial assistance</li>
+	<li>Organises events for Alumnis </li>
+	<li>Facilitate communication between Alumnis after graduation </li>
+	<li>Coordinate the assessment for graduation purposes</li>
+	<li>Organise activities to enhance graduate students research capabilities</li>
+</ul>
+</p>
+</div>
+</body>
+</html>
